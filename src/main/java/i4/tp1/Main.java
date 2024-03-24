55a8468024fcc00771058a5f06776818bdf6fbb9
SreypichTO <sreypichdarani2003@gmail.com>
2024-03-24 12:11:47 +0700

    change pom21:add jdk21

--- src/main/java/i4/tp1/Main.java
+++ src/main/java/i4/tp1/Main.java
@@ -2,6 +2,6 @@ package i4.tp1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test jdk 17 Lenkin sys: no jdk21");
+        System.out.println("change pom21:add jdk21");
     }
 }
